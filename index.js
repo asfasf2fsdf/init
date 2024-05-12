7e5801e13b0a6ef45d25b277d444f69ed9920ba5
asfasf2fsdf <gs1pamcass1@hotmail.com>
2024-05-13 06:55:54 +0700

    update sdk task

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-function countWords(str) {
-  return str.split(" ").filter((word) => word !== "").length;
-}
+const isPrime = (num) => {
+  for (let i = 2, sqrt = Math.sqrt(num); i <= sqrt; i++) {
+    if (num % i === 0) return false;
+  }
+  return num > 1;
+};
