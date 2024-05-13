29f33a561cd584d2958dc1c1623a4c9ba1b52c7c
asfasf2fsdf <gs1pamcass1@hotmail.com>
2024-05-13 11:53:38 +0700

    hello world

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-const isPrime = (num) => {
-  for (let i = 2, sqrt = Math.sqrt(num); i <= sqrt; i++) {
-    if (num % i === 0) return false;
+function zigzagLevelOrder(root) {
+  if (!root) return [];
+  const result = [];
+  let isReverse = false;
+  const queue = [root];
+  while (queue.length) {
+    const size = queue.length;
+    const level = [];
+    for (let i = 0; i < size; i++) {
+      const node = queue.shift();
+      if (isReverse) {
+        level.unshift(node.val);
+      } else {
+        level.push(node.val);
+      }
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    result.push(level);
+    isReverse = !isReverse;
   }
-  return num > 1;
-};
+  return result;
+}
